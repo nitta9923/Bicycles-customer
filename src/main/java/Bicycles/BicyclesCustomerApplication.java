5eaddf40924ac4e2578fd86bff213cd67f962ea5
nitta <nitta@nittanoMacBook-Pro.local>
2024-07-15 20:08:46 +0900

    初期コミット

---
+++ src/main/java/Bicycles/BicyclesCustomerApplication.java
@@ -0,0 +1,13 @@
+package Bicycles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicyclesCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BicyclesCustomerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Bicycles/BicyclesCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package Bicycles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicyclesCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
